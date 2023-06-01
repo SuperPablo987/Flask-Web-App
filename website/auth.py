313715776bc8003aab90a6055b850855fc7f4b67
Peter Thiel <thiel@ualberta.ca>
2023-06-01 16:04:20 -0600

    initial commit

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,16 @@ from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
+
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -29,5 +32,4 @@ def sign_up():
         else:
             flash('Account Created', category='success')
     
-
     return render_template("sign_up.html")
